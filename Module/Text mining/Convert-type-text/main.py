7b97b0138844b0e04b5a9abcc81000eaf5ae9615
aliasgharmirhshai <lmyrshahy3@gmail.com>
2022-06-16 23:22:15 +0430

    Config Python FIle

--- Module/Text mining/Convert-type-text/main.py
+++ Module/Text mining/Convert-type-text/main.py
@@ -1,20 +1,22 @@
 from PIL import Image
 import pytesseract
 import pathlib
+import os
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 text = ""
 
 # Persion Pics Input
-for path in pathlib.Path(r"H:\My Life\Data\Example\convert\farsi").iterdir():
+#for path in pathlib.Path(r"H:\My Life\Data\Example\image-to-text\Module\Text mining\Convert-type-text\farsi").iterdir():
+for path in pathlib.Path(fr"farsi").iterdir():
     if path.is_file():
         img = path
         text += pytesseract.image_to_string(Image.open(img), lang="fas")
         text += 50 * "_"
 
 # English Pics Input
-for path in pathlib.Path(r"H:\My Life\Data\Example\convert\english").iterdir():
+for path in pathlib.Path(r"english").iterdir():
     if path.is_file():
         img = path
         eng = pytesseract.image_to_string(Image.open(img), lang="eng")
@@ -22,11 +24,12 @@ for path in pathlib.Path(r"H:\My Life\Data\Example\convert\english").iterdir():
         text += 50 * "_"
 
 # Create Persion Text File
-with open("farsi.txt", "w", encoding="utf-8") as f:
+with open("result/farsi.txt", "w", encoding="utf-8") as f:
     f.write(text)
 
 # Create English Text File
-with open("english.txt", "w", encoding="utf-8") as f:
+with open("result/english.txt", "w", encoding="utf-8") as f:
     f.write(eng)
+
 
 
